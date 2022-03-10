9216fb5bb6589e17fa65bc2b69e1923ffdcb4453
wangkobong <wangkobong@gmail.com>
2022-03-10 15:53:00 +0900

    second commit

---
+++ Spotify_clone/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Spotify_clone/Controllers/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/10.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
