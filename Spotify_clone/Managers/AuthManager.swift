4babac886130188cd25c65f4244d80ef8fa5d36d
wangkobong <wangkobong@gmail.com>
2022-03-10 22:34:52 +0900

    AuthViewController에 웹뷰 추가

--- Spotify_clone/Managers/AuthManager.swift
+++ Spotify_clone/Managers/AuthManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientID = "9765b03be5204ff6a6120aa1906ec9e9"
+        static let clientSecret = "00665d77172e4b5e80b966d8760356b6"
+    }
+    
+    private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+    
+    var isSignedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String? {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool {
+        return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        // Get Token
+    }
+    
+    private func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
+    }
+}
---
+++ Spotify_clone/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extension.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/10.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+    
+
+}
