ff4e0fc5a57886f2a7343e44070ad0435acde69c
wangkobong <wangkobong@gmail.com>
2022-03-16 01:36:34 +0900

    AlbumViewController 추가

---
+++ Spotify_clone/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/16.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
---
+++ Spotify_clone/Views/AlbumTrackCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  AlbumTrackCollectionViewCell.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/16.
+//
+
+import UIKit
+
+class AlbumTrackCollectionViewCell: UICollectionViewCell {
+    static let identifier = "AlbumTrackCollectionViewCell"
+
+    
+    private let trackNameLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .secondarySystemBackground
+        [trackNameLabel, artistNameLabel].forEach {
+            contentView.addSubview($0)
+        }
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        trackNameLabel.frame = CGRect(
+            x: 10,
+            y: 0,
+            width: contentView.width-15,
+            height: contentView.height/2)
+        
+        artistNameLabel.frame = CGRect(
+            x: 10,
+            y: contentView.height/2,
+            width: contentView.width-15,
+            height: contentView.height/2)
+
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        trackNameLabel.text = nil
+        artistNameLabel.text = nil
+    }
+    
+    func configure(with viewModel: AlbumCollectionViewCellViewModel) {
+        trackNameLabel.text = viewModel.name
+        artistNameLabel.text = viewModel.artistName
+        
+    }
+}
