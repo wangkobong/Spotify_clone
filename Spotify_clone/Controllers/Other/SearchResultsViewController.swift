f3560b50a4734e3ec3db1c286bd004194c436fd4
wangkobong <wangkobong@gmail.com>
2022-03-16 23:42:50 +0900

    SearchController에서치바 및 콜렉션뷰 추가

---
+++ Spotify_clone/Controllers/Other/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/16.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+}
