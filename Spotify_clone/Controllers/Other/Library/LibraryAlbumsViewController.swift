8c93a1747af87b5c49ac16d5560ca111232051ba
wangkobong <wangkobong@gmail.com>
2022-03-19 23:49:14 +0900

    LibraryViewController 기본 화면 구성

---
+++ Spotify_clone/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/19.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+    
+}
---
+++ Spotify_clone/Controllers/Other/Library/LibraryPlaylistViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryPlaylistViewController.swift
+//  Spotify_clone
+//
+//  Created by sungyeon kim on 2022/03/19.
+//
+
+import UIKit
+
+class LibraryPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+
+}
